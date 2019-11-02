879c7b71bb120e1667c902b9e9a119087188cdb1
Diego Batista <diego.silva@aluno.ic.ufmt.br>
2019-11-01 21:06:04 -0400

    Add files via upload

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'hello world fdp!!!' });
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Diego Batista',
+        email: 'diego.silva@aluno.ic.ufmt.br',
+        password_hash: 'd1d2d3d4d5d6',
+    });
+
+    return res.json(user);
 });
 
 export default routes;
